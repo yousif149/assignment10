e4e6899062b2aa92006617940b332663012caae3
yousif14 <tameryousif336@gmail.com>
2024-11-21 00:13:17 +0200

    git file

---
+++ index.js
@@ -0,0 +1,21 @@
+//Use an array to hold the value of the quotes
+const arrayOfQuotes = [
+    {'author': 'Oscar Wilde', 
+     'quote': 'Be yourself; everyone else is already taken'
+    },
+    {'author': ' Marilyn Monroe', 
+     'quote': 'Im selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you cant handle me at my worst, then you sure as hell dont deserve me at my best.'
+    },
+    {'author': 'Frank Zappa', 
+     'quote': 'So many books, so little time.'
+    },
+    {'author': 'Albert Einstein', 
+     'quote': 'Two things are infinite: the universe and human stupidity; and Im not sure about the universe.'
+    },
+    {'author': 'Nelson Mandela', 
+     'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
+    },
+    {'author': 'Elbert Hubbard', 
+     'quote': 'Do not take life too seriously. You will not get out alive.'
+    },
+];
